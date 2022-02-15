059801b2e5b4fb5a97438f581ed9189e2e6651ab
Aman Sharma <mannu.poski10@gmail.com>
2022-02-15 11:42:25 +0100

    Get inspired from Sorald

--- deploy-spoon-maven-central.sh
+++ deploy-spoon-maven-central.sh
@@ -8,10 +8,6 @@
 # Install dependencies
 sudo apt-get install -y xmlstarlet
 
-# Fetch SpoonBot GPG key
-gpg --batch --pinentry-mode=loopback --fast-import --passphrase="$SPOONBOT_PASSPHRASE" spoonbot.gpg
-KEY=`gpg --list-keys --with-colons | grep pub | cut -f5 -d: | tail -1`
-
 git clone https://github.com/INRIA/spoon/
 
 cd spoon
@@ -46,7 +42,7 @@ CURRENT_VERSION=`xmlstarlet sel -t -v '/_:project/_:version' pom.xml`
 CURRENT_VERSION_NO_SNAPSHOT=`echo $CURRENT_VERSION | sed -e 's/-SNAPSHOT//'`
 echo CURRENT_VERSION_NO_SNAPSHOT $CURRENT_VERSION_NO_SNAPSHOT
 xmlstarlet edit -L --update '/_:project/_:version' --value $CURRENT_VERSION_NO_SNAPSHOT pom.xml
-mvn -q clean deploy -DskipTests --settings ../settings.xml -Prelease -DskipStaging=true -Dgpg.keyname=$KEY -DadditionalJOption=-Xdoclint:none
+mvn clean deploy -DskipTests -Prelease
 if [ $? -eq 0 ]; then
     echo pushing tag on github
     git checkout -b $CURRENT_VERSION_NO_SNAPSHOT
@@ -80,4 +76,4 @@ echo NEW_BETA_NUMBER $NEW_BETA_NUMBER
 PUSHED_VERSION=$CURRENT_VERSION_NO_SNAPSHOT-beta-$NEW_BETA_NUMBER
 echo deploying $PUSHED_VERSION
 xmlstarlet edit -L --update '/_:project/_:version' --value $PUSHED_VERSION pom.xml
-mvn -q clean deploy -DskipTests --settings ../settings.xml -Prelease -DskipStaging=true -Dgpg.keyname=$KEY -DadditionalJOption=-Xdoclint:none
+mvn clean deploy -DskipTests -Prelease
