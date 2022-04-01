21493c177babe401288c08bc5e48ac9b21498a92
Martin Monperrus <martin.monperrus@gnieh.org>
2022-04-01 08:36:39 +0000

    decouple beta releases from official releases

--- deploy-spoon-maven-central.sh
+++ deploy-spoon-maven-central.sh
@@ -30,27 +30,7 @@ cd ..
 # Prevent error: "gpg: signing failed: Inappropriate ioctl for device"
 export GPG_TTY=$(tty)
 
-# we do a normal release at the last bump commit
-# this works the first time and will fail after
-git reset --hard # clean
-LAST_BUMP_COMMIT=`git --no-pager log --format=format:%H  -L 21,21:pom.xml | head -1`
-echo LAST_BUMP_COMMIT $LAST_BUMP_COMMIT
-git checkout $LAST_BUMP_COMMIT^1 # checking out the commit just before the bump
-quick_fix_pom
-xmlstarlet edit -L --update '/_:project/_:description' --value `git rev-parse HEAD` pom.xml
-CURRENT_VERSION=`xmlstarlet sel -t -v '/_:project/_:version' pom.xml`
-CURRENT_VERSION_NO_SNAPSHOT=`echo $CURRENT_VERSION | sed -e 's/-SNAPSHOT//'`
-echo CURRENT_VERSION_NO_SNAPSHOT $CURRENT_VERSION_NO_SNAPSHOT
-xmlstarlet edit -L --update '/_:project/_:version' --value $CURRENT_VERSION_NO_SNAPSHOT pom.xml
-mvn -q clean deploy -DskipTests -Prelease
-if [ $? -eq 0 ]; then
-    echo pushing tag on github
-    git checkout -b $CURRENT_VERSION_NO_SNAPSHOT
-    git tag spoon-core-$CURRENT_VERSION_NO_SNAPSHOT
-    git push origin
-
-    # TODO create github release
-fi
+## now, official releases are handled by a separate script
 
 # now we release a beta version
 git reset --hard # clean
